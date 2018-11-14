111c30d931d6823d74746e765574d68987a38c26
renz <renz@snapzio.com>
2018-11-14 08:07:09 +0800

    change text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Snapzio
+            Snapzio Rocks
           </p>
           <a
             className="App-link"
